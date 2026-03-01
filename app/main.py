e3c2f9837e8271ed2684c62fedbd21933245e46b
ahmadmm01 <skyahmad74@gmail.com>
2026-03-01 20:02:45 +0700

    update origins

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="iSENSAir Data Logger API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://isensair.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
